a97d00dcd44e0f2968d009b1a7dcac94427ff599
MakowToms <tomasz.tu.tm@gmail.com>
2023-12-09 16:38:34 +0100

    feat(grade): improve student grade view

--- by_student/urls.py
+++ by_student/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 
 from .views import (
-    by_student, by_student_edit, by_student_pick_topic,
+    by_student, by_student_edit,
     by_student_update, by_student_view,
 )
 
 urlpatterns = [
     path("", by_student),
-    path("<int:pk>/", by_student_pick_topic),
-    path("<int:pk1>/topic/<int:pk2>/", by_student_view),
+    path("<int:pk1>/", by_student_view),
     path("<int:pk1>/topic/<int:pk2>/update/", by_student_update),
     path("<int:pk1>/topic/<int:pk2>/edit/", by_student_edit),
 ]
